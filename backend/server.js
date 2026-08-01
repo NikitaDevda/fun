af2f29c4f8b14285b593f54cecd108fe8994fd9a
Nikita Devda <nikitadevda178@gmail.com>
2026-08-01 16:01:03 +0530

    Mongoose install aur setup kiya

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const User = require('./models/User');
+const User = require('../models/User');
 
 dotenv.config();
 

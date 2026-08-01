4f5bf4820ee9c9d1946648f86c856544710dec81
NikitaDevda <nikitadevda178@gmail.com>
2026-08-01 16:24:39 +0530

    Update health.js

--- api/health.js
+++ api/health.js
@@ -1,13 +1,4 @@
 module.exports = (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-
-  if (req.method === 'OPTIONS') {
-    res.status(200).end();
-    return;
-  }
-
   res.status(200).json({
     status: 'OK',
     message: 'Vercel API is running!',
